96f778d8f3453e9e3932d123cf495fcae6b2b135
main <plvdev@proton.me>
2025-11-10 15:28:25 +0100

    feat(docs): enhance documentation and add new content
    - Updated the documentation to include new sections for "Database" and "ORM" with relevant content.
    - Added "Extras" section in the documentation, featuring "biomejs" and "husky".
    - Improved the "Expo" and "Hono" documentation with detailed descriptions and technical changes.
    - Enhanced the "Next.js" documentation with additional features and modules, including MDX and PWA support.
    - Updated the pre-commit hook to conditionally run formatting based on the extras configuration.

--- apps/www/.source/index.ts
+++ apps/www/.source/index.ts
@@ -1,7 +1,13 @@
 // @ts-nocheck -- skip type checking
-import * as d_docs_8 from '../content/docs/stacks/nextjs.mdx?collection=docs';
-import * as d_docs_7 from '../content/docs/stacks/hono.mdx?collection=docs';
-import * as d_docs_6 from '../content/docs/stacks/expo.mdx?collection=docs';
+import * as d_docs_14 from '../content/docs/extras/husky.mdx?collection=docs';
+import * as d_docs_13 from '../content/docs/extras/biomejs.mdx?collection=docs';
+import * as d_docs_12 from '../content/docs/database/postgresql.mdx?collection=docs';
+import * as d_docs_11 from '../content/docs/database/mysql.mdx?collection=docs';
+import * as d_docs_10 from '../content/docs/stacks/nextjs.mdx?collection=docs';
+import * as d_docs_9 from '../content/docs/stacks/hono.mdx?collection=docs';
+import * as d_docs_8 from '../content/docs/stacks/expo.mdx?collection=docs';
+import * as d_docs_7 from '../content/docs/orm/prisma.mdx?collection=docs';
+import * as d_docs_6 from '../content/docs/orm/drizzle.mdx?collection=docs';
 import * as d_docs_5 from '../content/docs/why.mdx?collection=docs';
 import * as d_docs_4 from '../content/docs/roadmap.mdx?collection=docs';
 import * as d_docs_3 from '../content/docs/options.mdx?collection=docs';
@@ -18,9 +24,15 @@ export const docs = _runtime.docs<typeof _source.docs>(
     { info: { path: 'options.mdx', fullPath: 'content/docs/options.mdx' }, data: d_docs_3 },
     { info: { path: 'roadmap.mdx', fullPath: 'content/docs/roadmap.mdx' }, data: d_docs_4 },
     { info: { path: 'why.mdx', fullPath: 'content/docs/why.mdx' }, data: d_docs_5 },
-    { info: { path: 'stacks/expo.mdx', fullPath: 'content/docs/stacks/expo.mdx' }, data: d_docs_6 },
-    { info: { path: 'stacks/hono.mdx', fullPath: 'content/docs/stacks/hono.mdx' }, data: d_docs_7 },
-    { info: { path: 'stacks/nextjs.mdx', fullPath: 'content/docs/stacks/nextjs.mdx' }, data: d_docs_8 },
+    { info: { path: 'orm/drizzle.mdx', fullPath: 'content/docs/orm/drizzle.mdx' }, data: d_docs_6 },
+    { info: { path: 'orm/prisma.mdx', fullPath: 'content/docs/orm/prisma.mdx' }, data: d_docs_7 },
+    { info: { path: 'stacks/expo.mdx', fullPath: 'content/docs/stacks/expo.mdx' }, data: d_docs_8 },
+    { info: { path: 'stacks/hono.mdx', fullPath: 'content/docs/stacks/hono.mdx' }, data: d_docs_9 },
+    { info: { path: 'stacks/nextjs.mdx', fullPath: 'content/docs/stacks/nextjs.mdx' }, data: d_docs_10 },
+    { info: { path: 'database/mysql.mdx', fullPath: 'content/docs/database/mysql.mdx' }, data: d_docs_11 },
+    { info: { path: 'database/postgresql.mdx', fullPath: 'content/docs/database/postgresql.mdx' }, data: d_docs_12 },
+    { info: { path: 'extras/biomejs.mdx', fullPath: 'content/docs/extras/biomejs.mdx' }, data: d_docs_13 },
+    { info: { path: 'extras/husky.mdx', fullPath: 'content/docs/extras/husky.mdx' }, data: d_docs_14 },
   ],
   [
     {
@@ -39,6 +51,15 @@ export const docs = _runtime.docs<typeof _source.docs>(
           'stacks/nextjs',
           'stacks/expo',
           'stacks/hono',
+          '---Database---',
+          'database/postgresql',
+          'database/mysql',
+          '---ORM---',
+          'orm/drizzle',
+          'orm/prisma',
+          '---Extras---',
+          'extras/biomejs',
+          'extras/husky',
         ],
         description: 'Documentation for the project',
         root: true,
